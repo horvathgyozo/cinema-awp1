762573f8184d28907942342236e1a56ca493b6a0
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2025-04-07 16:45:44 +0200

    added shadcn/ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <h1>Hello world!</h1>
-  )
+    <>
+      <h1>Hello world!</h1>
+      <Button disabled>
+        <Loader2 className="animate-spin" />
+        Please wait
+      </Button>
+    </>
+  );
 }
 
-export default App
+export default App;
